54c4cb6f866976b30a137673164fbe31255307f9
Miljan Šljivić <miljan.sljivic@novalite.rs>
2023-05-08 09:55:33 +0200

    Add DateTimePickerFormControl

---
+++ Enigmatry.Entry.CodeGeneration.Configuration/Form/Controls/DateTimePicker/DateTimePickerFormControlBuilder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reflection;
+
+namespace Enigmatry.Entry.CodeGeneration.Configuration.Form.Controls;
+
+public class DateTimePickerFormControlBuilder :
+    BaseControlBuilder<DateTimePickerFormControl, DateTimePickerFormControlBuilder>
+{
+    private DateTimeOffset? _defaultValue;
+
+    public DateTimePickerFormControlBuilder(PropertyInfo propertyInfo) : base(propertyInfo)
+    {
+    }
+
+    public DateTimePickerFormControlBuilder(string propertyName) : base(propertyName)
+    {
+    }
+
+    public DateTimePickerFormControlBuilder WithDefaultValue(DateTimeOffset defaultValue)
+    {
+        _defaultValue = defaultValue;
+        return this;
+    }
+
+    public override FormControl Build(ComponentInfo componentInfo)
+    {
+        var dateTimePickerFormControl = new DateTimePickerFormControl { DefaultValue = _defaultValue };
+        return Build(componentInfo, dateTimePickerFormControl);
+    }
+}
--- Enigmatry.Entry.CodeGeneration.Configuration/Form/Controls/FormlyTypes.cs
+++ Enigmatry.Entry.CodeGeneration.Configuration/Form/Controls/FormlyTypes.cs
@@ -5,6 +5,7 @@ public static class FormlyTypes
     public const string Input = "input";
     public const string CheckBox = "checkbox";
     public const string DatePicker = "datepicker";
+    public const string DateTimePicker = "datetimepicker";
     public const string Radio = "radio";
     public const string Select = "select";
     public const string Autocomplete = "autocomplete";
