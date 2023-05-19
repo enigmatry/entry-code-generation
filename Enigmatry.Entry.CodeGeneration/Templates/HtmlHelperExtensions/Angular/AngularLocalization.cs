a545fd9c48f4800bb3647b5b9415eef2c83743b0
Marko Maric <marko.maric@novalite.rs>
2023-05-19 10:33:25 +0200

    Replace ' with ` in formatted un-locallized strings

--- Enigmatry.Entry.CodeGeneration/Templates/HtmlHelperExtensions/Angular/AngularLocalization.cs
+++ Enigmatry.Entry.CodeGeneration/Templates/HtmlHelperExtensions/Angular/AngularLocalization.cs
@@ -9,12 +9,12 @@ public static class AngularLocalization
 {
     public static IHtmlContent Localize(this IHtmlHelper html, string key, string text, bool enableI18N)
     {
-        return enableI18N && text.HasContent() ? html.Raw(Localize(key, text)) : html.Raw($"'{text}'");
+        return enableI18N && text.HasContent() ? html.Raw(Localize(key, text)) : html.Raw($"`{text}`");
     }
 
     public static IHtmlContent Localize(this IHtmlHelper html, I18NString? text, bool enableI18N)
     {
-        return text != null && enableI18N && text.HasContent() ? html.Raw(Localize(text.Key, text.Value)) : html.Raw($"'{text?.Value ?? String.Empty}'");
+        return text != null && enableI18N && text.HasContent() ? html.Raw(Localize(text.Key, text.Value)) : html.Raw($"`{text?.Value ?? String.Empty}`");
     }
 
     public static string Localize(string key, string value)
