ed1e583da215d341f5dca242c379fc7f90d613c7
skljaic <skl@enigmatry.com>
2023-10-31 09:07:52 +0100

    Align pageSize between PaginationInfo and PaginationInfoBuilder to accomodate available size on modern monitors. (#44)

--- Enigmatry.Entry.CodeGeneration.Configuration/List/Model/PaginationInfo.cs
+++ Enigmatry.Entry.CodeGeneration.Configuration/List/Model/PaginationInfo.cs
@@ -6,7 +6,7 @@ public class PaginationInfo
 {
     public bool ShowPaginator { get; set; } = true;
     public bool ShowFirstLastPageButtons { get; set; } = true;
-    public int PageSize { get; set; } = 10;
-    public IEnumerable<int> PageSizeOptions { get; set; } = new[] { 10, 50, 100 };
+    public int PageSize { get; set; } = 20;
+    public IEnumerable<int> PageSizeOptions { get; set; } = new[] { 20, 50, 100 };
     public bool ShowPageSize { get; set; } = true;
 }
