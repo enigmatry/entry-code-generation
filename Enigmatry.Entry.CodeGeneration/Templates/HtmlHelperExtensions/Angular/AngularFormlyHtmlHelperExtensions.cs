64602325e0b8610e5de4e7cefbfb8fcaf4265115
Nikola Dragićević <nda@enigmatry.com>
2026-02-06 00:15:56 +0100

    Fixing .net8 build.

--- Enigmatry.Entry.CodeGeneration/Templates/HtmlHelperExtensions/Angular/AngularFormlyHtmlHelperExtensions.cs
+++ Enigmatry.Entry.CodeGeneration/Templates/HtmlHelperExtensions/Angular/AngularFormlyHtmlHelperExtensions.cs
@@ -8,39 +8,36 @@ namespace Enigmatry.Entry.CodeGeneration.Templates.HtmlHelperExtensions.Angular;
 
 public static class AngularFormlyHtmlHelperExtensions
 {
-    extension(IHtmlHelper html)
+    public static IHtmlContent FieldCssClass(this IHtmlHelper html, FormControl control) => html.Raw($"className: `{control.StackedClasses()}`,\r\n");
+
+    public static IHtmlContent GroupCssClass(this IHtmlHelper html, FormControlGroup controlGroup)
     {
-        public IHtmlContent FieldCssClass(FormControl control) => html.Raw($"className: `{control.StackedClasses()}`,\r\n");
+        var classNameValue = controlGroup.ClassNames.Values.Aggregate("entry-field-group",
+            (current, className) => current + $" {className.ApplyOptionally()}");
 
-        public IHtmlContent GroupCssClass(FormControlGroup controlGroup)
-        {
-            var classNameValue = controlGroup.ClassNames.Values.Aggregate("entry-field-group", 
-                (current, className) => current + $" {className.ApplyOptionally()}");
-
-            return html.Raw($"fieldGroupClassName: `{classNameValue}`,\r\n");
-        }
-
-        public IHtmlContent DefaultValue(FormControl control) =>
-            control switch
-            {
-                DatepickerFormControl formControl => html.RenderDefaultValue(formControl.DefaultValue),
-                InputControlBase formControl => html.RenderDefaultValue(formControl.DefaultValue),
-                CheckboxFormControl formControl => html.RenderDefaultValue(formControl.DefaultValue),
-                RadioGroupFormControl formControl => html.RenderDefaultValue(formControl.DefaultValue),
-                SelectFormControl formControl => html.RenderDefaultValue(formControl.DefaultValue),
-                _ => html.Raw("")
-            };
-
-        private IHtmlContent RenderDefaultValue(bool? defaultValue) => 
-            defaultValue.HasValue ? html.Raw($"defaultValue: {(defaultValue.Value ? "true" : "false")},\r\n") : html.Raw("");
-
-        private IHtmlContent RenderDefaultValue(DateTimeOffset? defaultValue) =>
-            defaultValue.HasValue
-                // O - ISO 8601
-                ? html.Raw($"defaultValue: '{defaultValue.Value.ToString("O", CultureInfo.InvariantCulture)}',\r\n")
-                : html.Raw("");
-
-        private IHtmlContent RenderDefaultValue(string? defaultValue) => 
-            defaultValue.HasContent() ? html.Raw($"defaultValue: '{defaultValue}',\r\n") : html.Raw("");
+        return html.Raw($"fieldGroupClassName: `{classNameValue}`,\r\n");
     }
+
+    public static IHtmlContent DefaultValue(this IHtmlHelper html, FormControl control) =>
+        control switch
+        {
+            DatepickerFormControl formControl => html.RenderDefaultValue(formControl.DefaultValue),
+            InputControlBase formControl => html.RenderDefaultValue(formControl.DefaultValue),
+            CheckboxFormControl formControl => html.RenderDefaultValue(formControl.DefaultValue),
+            RadioGroupFormControl formControl => html.RenderDefaultValue(formControl.DefaultValue),
+            SelectFormControl formControl => html.RenderDefaultValue(formControl.DefaultValue),
+            _ => html.Raw("")
+        };
+
+    private static IHtmlContent RenderDefaultValue(this IHtmlHelper html, bool? defaultValue) =>
+        defaultValue.HasValue ? html.Raw($"defaultValue: {(defaultValue.Value ? "true" : "false")},\r\n") : html.Raw("");
+
+    private static IHtmlContent RenderDefaultValue(this IHtmlHelper html, DateTimeOffset? defaultValue) =>
+        defaultValue.HasValue
+            // O - ISO 8601
+            ? html.Raw($"defaultValue: '{defaultValue.Value.ToString("O", CultureInfo.InvariantCulture)}',\r\n")
+            : html.Raw("");
+
+    private static IHtmlContent RenderDefaultValue(this IHtmlHelper html, string? defaultValue) =>
+        defaultValue.HasContent() ? html.Raw($"defaultValue: '{defaultValue}',\r\n") : html.Raw("");
 }
