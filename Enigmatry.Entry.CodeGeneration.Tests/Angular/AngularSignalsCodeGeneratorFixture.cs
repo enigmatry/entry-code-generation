ba9737ac21ec68175a7d7505e0178408dec39c65
Nikola Dragićević <nda@enigmatry.com>
2026-06-09 01:45:37 +0200

    Eliminated any from signals.

--- Enigmatry.Entry.CodeGeneration.Tests/Angular/AngularSignalsCodeGeneratorFixture.cs
+++ Enigmatry.Entry.CodeGeneration.Tests/Angular/AngularSignalsCodeGeneratorFixture.cs
@@ -30,18 +30,4 @@ public class AngularSignalsCodeGeneratorFixture : AngularCodeGeneratorFixtureBas
         AssertGeneratedFileContent(inMemoryTemplateWriter.FilesToWrite);
     }
 
-    [Test]
-    public async Task CaptureSnapshots()
-    {
-        await _codeGenerator.Generate();
-        var inMemoryTemplateWriter = (InMemoryTemplateWriter)GetService<ITemplateWriter>();
-
-        foreach (var (path, content) in inMemoryTemplateWriter.FilesToWrite)
-        {
-            var fileName = $"{path.Split(Path.DirectorySeparatorChar).Last()}.txt";
-            var snapshotPath = Path.Combine(ExpectedResultsLocation, fileName);
-            await File.WriteAllTextAsync(snapshotPath, content);
-            Console.WriteLine($"Updated snapshot: {snapshotPath}");
-        }
-    }
 }
