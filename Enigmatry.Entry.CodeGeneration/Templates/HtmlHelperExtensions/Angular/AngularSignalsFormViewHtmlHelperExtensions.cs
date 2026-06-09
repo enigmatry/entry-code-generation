e7834b43a78f7a67f276335c2b2992ffa6c0412b
Nikola Dragićević <nda@enigmatry.com>
2026-06-09 02:54:16 +0200

    Further clean up.

--- Enigmatry.Entry.CodeGeneration/Templates/HtmlHelperExtensions/Angular/AngularSignalsFormViewHtmlHelperExtensions.cs
+++ Enigmatry.Entry.CodeGeneration/Templates/HtmlHelperExtensions/Angular/AngularSignalsFormViewHtmlHelperExtensions.cs
@@ -8,61 +8,61 @@ namespace Enigmatry.Entry.CodeGeneration.Templates.HtmlHelperExtensions.Angular;
 
 public static class AngularSignalsFormViewHtmlHelperExtensions
 {
-    public static IHtmlContent RenderFormControls(this IHtmlHelper html, IEnumerable<FormControl> controls) =>
-        html.Raw(String.Concat(controls.Select(control => html.RenderFormControl(control).ToString())));
+    public static IHtmlContent RenderFormControls(this IHtmlHelper htmlHelper, IEnumerable<FormControl> controls) =>
+        htmlHelper.Raw(String.Concat(controls.Select(control => htmlHelper.RenderFormControl(control).ToString())));
 
-    private static IHtmlContent RenderFormControl(this IHtmlHelper html, FormControl control) => control switch
+    private static IHtmlContent RenderFormControl(this IHtmlHelper htmlHelper, FormControl control) => control switch
     {
-        FormControlGroup group => html.RenderFormControlGroup(group),
-        ButtonFormControl button => html.RenderFormButton(button),
-        _ => html.RenderFormField(control)
+        FormControlGroup group => htmlHelper.RenderFormControlGroup(group),
+        ButtonFormControl button => htmlHelper.RenderFormButton(button),
+        _ => htmlHelper.RenderFormField(control)
     };
 
-    private static IHtmlContent RenderFormControlGroup(this IHtmlHelper html, FormControlGroup group)
+    private static IHtmlContent RenderFormControlGroup(this IHtmlHelper htmlHelper, FormControlGroup group)
     {
         var staticClasses = group.ClassNames.Values
             .Where(classNameEntry => classNameEntry.When == ApplyWhen.Always)
             .Aggregate("entry-field-group", (current, classNameEntry) => current + $" {classNameEntry.Value}");
-        var conditionalParts = group.ClassNames.Values
+        var classBindings = String.Join("", group.ClassNames.Values
             .Where(classNameEntry => classNameEntry.When != ApplyWhen.Always)
             .Select(classNameEntry => classNameEntry.When == ApplyWhen.FormIsReadonly
-                ? $"'{classNameEntry.Value}': isReadonly()"
-                : $"'{classNameEntry.Value}': !isReadonly()")
-            .ToList();
-        var joinedConditions = String.Join(", ", conditionalParts);
-        var ngClassAttribute = conditionalParts.Any() ? $" [ngClass]=\"{{ {joinedConditions} }}\"" : "";
-        var innerContent = html.RenderFormControls(group.FormControls).ToString();
-        return html.Raw($"<div class=\"{staticClasses}\"{ngClassAttribute}>\r\n{innerContent}</div>\r\n");
+                ? $" [class.{classNameEntry.Value}]=\"isReadonly()\""
+                : $" [class.{classNameEntry.Value}]=\"!isReadonly()\""));
+        var innerContent = htmlHelper.RenderFormControls(group.FormControls).ToString();
+        return htmlHelper.Raw($"<div class=\"{staticClasses}\"{classBindings}>\r\n{innerContent}</div>\r\n");
     }
 
-    private static IHtmlContent RenderFormButton(this IHtmlHelper html, ButtonFormControl button) =>
-        html.Raw(
+    private static IHtmlContent RenderFormButton(this IHtmlHelper htmlHelper, ButtonFormControl button) =>
+        htmlHelper.Raw(
             $"<button mat-button type=\"button\"\r\n" +
             $"        class=\"{button.StackedClasses()}\"\r\n" +
             $"        [disabled]=\"isDisabled('{button.PropertyName}', {button.Readonly.ToString().ToLower()})\"\r\n" +
             $"        (click)=\"buttonClick.emit('{button.PropertyName}')\">{button.Text.Value}</button>\r\n");
 
-    private static IHtmlContent RenderFormField(this IHtmlHelper html, FormControl field)
+    private static IHtmlContent RenderFormField(this IHtmlHelper htmlHelper, FormControl field)
     {
-        if (!field.Visible) return html.Raw("");
+        if (!field.Visible)
+        {
+            return htmlHelper.Raw("");
+        }
 
         return field switch
         {
-            ArrayFormControl arrayControl => html.RenderArrayField(arrayControl),
-            RichTextInputFormControl richTextField => html.RenderRichTextField(richTextField),
-            DatepickerFormControl datepickerField => html.RenderDatepickerField(datepickerField),
-            DateTimePickerFormControl dateTimePickerField => html.RenderDateTimePickerField(dateTimePickerField),
-            MultiSelectFormControl multiSelectField => html.RenderMultiSelectField(multiSelectField),
-            SelectFormControl selectField => html.RenderSelectField(selectField),
-            MultiCheckboxFormControl multiCheckboxField => html.RenderMultiCheckboxField(multiCheckboxField),
-            RadioGroupFormControl radioGroupField => html.RenderRadioGroupField(radioGroupField),
-            CheckboxFormControl checkboxField => html.RenderCheckboxField(checkboxField),
-            InputControlBase inputField => html.RenderInputField(inputField),
-            _ => html.RenderGenericField(field)
+            ArrayFormControl arrayControl => htmlHelper.RenderArrayField(arrayControl),
+            RichTextInputFormControl richTextField => htmlHelper.RenderRichTextField(richTextField),
+            DatepickerFormControl datepickerField => htmlHelper.RenderDatepickerField(datepickerField),
+            DateTimePickerFormControl dateTimePickerField => htmlHelper.RenderDateTimePickerField(dateTimePickerField),
+            MultiSelectFormControl multiSelectField => htmlHelper.RenderMultiSelectField(multiSelectField),
+            SelectFormControl selectField => htmlHelper.RenderSelectField(selectField),
+            MultiCheckboxFormControl multiCheckboxField => htmlHelper.RenderMultiCheckboxField(multiCheckboxField),
+            RadioGroupFormControl radioGroupField => htmlHelper.RenderRadioGroupField(radioGroupField),
+            CheckboxFormControl checkboxField => htmlHelper.RenderCheckboxField(checkboxField),
+            InputControlBase inputField => htmlHelper.RenderInputField(inputField),
+            _ => htmlHelper.RenderGenericField(field)
         };
     }
 
-    private static IHtmlContent RenderInputField(this IHtmlHelper html, InputControlBase field)
+    private static IHtmlContent RenderInputField(this IHtmlHelper htmlHelper, InputControlBase field)
     {
         var inputType = field.Type ?? "text";
         var appearanceAttribute = field.Appearance.HasValue
@@ -79,12 +79,12 @@ public static class AngularSignalsFormViewHtmlHelperExtensions
         var hintLine = field.Hint.Value.HasContent()
             ? $"    <mat-hint>{field.Hint.Value}</mat-hint>\r\n"
             : "";
-        var validationErrors = String.Concat(field.ValidationRules.Select(rule =>
-            $"@if (form.get('{field.PropertyName}')?.hasError('{rule.FormlyRuleName}')) {{\r\n" +
-            $"    <mat-error>{(rule.HasCustomMessage ? rule.CustomMessage : rule.FormlyValidationMessage)}</mat-error>\r\n" +
+        var validationErrors = String.Concat(field.ValidationRules.Select(validationRule =>
+            $"@if (form.get('{field.PropertyName}')?.hasError('{validationRule.FormlyRuleName}')) {{\r\n" +
+            $"    <mat-error>{(validationRule.HasCustomMessage ? validationRule.CustomMessage : validationRule.FormlyValidationMessage)}</mat-error>\r\n" +
             $"}}"));
 
-        return html.Raw(
+        return htmlHelper.Raw(
             $"@if (!isHidden('{field.PropertyName}', {field.Visible.ToString().ToLower()})) {{\r\n" +
             $"<mat-form-field class=\"{field.StackedClasses()}\"{appearanceAttribute}{floatLabelAttribute}>\r\n" +
             $"    <mat-label>{field.Label.Value}</mat-label>\r\n" +
@@ -95,23 +95,23 @@ public static class AngularSignalsFormViewHtmlHelperExtensions
             $"}}\r\n");
     }
 
-    private static IHtmlContent RenderCheckboxField(this IHtmlHelper html, CheckboxFormControl field) =>
-        html.Raw(
+    private static IHtmlContent RenderCheckboxField(this IHtmlHelper htmlHelper, CheckboxFormControl field) =>
+        htmlHelper.Raw(
             $"@if (!isHidden('{field.PropertyName}', {field.Visible.ToString().ToLower()})) {{\r\n" +
             $"<mat-checkbox formControlName=\"{field.PropertyName}\" class=\"{field.StackedClasses()}\">{field.Label.Value}</mat-checkbox>\r\n" +
             $"}}\r\n");
 
-    private static IHtmlContent RenderSelectField(this IHtmlHelper html, SelectFormControl field)
+    private static IHtmlContent RenderSelectField(this IHtmlHelper htmlHelper, SelectFormControl field)
     {
         var appearanceAttribute = field.Appearance.HasValue
             ? $" appearance=\"{field.Appearance!.Value.ToString().ToLower()}\""
             : "";
-        return html.Raw(
+        return htmlHelper.Raw(
             $"@if (!isHidden('{field.PropertyName}', {field.Visible.ToString().ToLower()})) {{\r\n" +
             $"<mat-form-field class=\"{field.StackedClasses()}\"{appearanceAttribute}>\r\n" +
             $"    <mat-label>{field.Label.Value}</mat-label>\r\n" +
             $"    <mat-select formControlName=\"{field.PropertyName}\">\r\n" +
-            $"        @for (let option of {field.PropertyName}Options(); track option.value) {{\r\n" +
+            $"        @for (option of {field.PropertyName}Options(); track option.value) {{\r\n" +
             $"            <mat-option [value]=\"option.value\">{{{{option.displayName}}}}</mat-option>\r\n" +
             $"        }}\r\n" +
             $"    </mat-select>\r\n" +
@@ -119,7 +119,7 @@ public static class AngularSignalsFormViewHtmlHelperExtensions
             $"}}\r\n");
     }
 
-    private static IHtmlContent RenderMultiSelectField(this IHtmlHelper html, MultiSelectFormControl field)
+    private static IHtmlContent RenderMultiSelectField(this IHtmlHelper htmlHelper, MultiSelectFormControl field)
     {
         var selectAllOption = field.Options.SelectAllOption;
         var appearanceAttribute = field.Appearance.HasValue
@@ -128,13 +128,13 @@ public static class AngularSignalsFormViewHtmlHelperExtensions
         var selectAllLine = selectAllOption != null
             ? $"        <mat-option>{selectAllOption.DisplayName.Value}</mat-option>\r\n"
             : "";
-        return html.Raw(
+        return htmlHelper.Raw(
             $"@if (!isHidden('{field.PropertyName}', {field.Visible.ToString().ToLower()})) {{\r\n" +
             $"<mat-form-field class=\"{field.StackedClasses()}\"{appearanceAttribute}>\r\n" +
             $"    <mat-label>{field.Label.Value}</mat-label>\r\n" +
             $"    <mat-select formControlName=\"{field.PropertyName}\" multiple>\r\n" +
             selectAllLine +
-            $"        @for (let option of {field.PropertyName}Options(); track option.value) {{\r\n" +
+            $"        @for (option of {field.PropertyName}Options(); track option.value) {{\r\n" +
             $"            <mat-option [value]=\"option.value\">{{{{option.displayName}}}}</mat-option>\r\n" +
             $"        }}\r\n" +
             $"    </mat-select>\r\n" +
@@ -142,27 +142,27 @@ public static class AngularSignalsFormViewHtmlHelperExtensions
             $"}}\r\n");
     }
 
-    private static IHtmlContent RenderRadioGroupField(this IHtmlHelper html, RadioGroupFormControl field) =>
-        html.Raw(
+    private static IHtmlContent RenderRadioGroupField(this IHtmlHelper htmlHelper, RadioGroupFormControl field) =>
+        htmlHelper.Raw(
             $"@if (!isHidden('{field.PropertyName}', {field.Visible.ToString().ToLower()})) {{\r\n" +
             $"<mat-radio-group formControlName=\"{field.PropertyName}\" class=\"{field.StackedClasses()}\">\r\n" +
-            $"    @for (let option of {field.PropertyName}Options(); track option.value) {{\r\n" +
+            $"    @for (option of {field.PropertyName}Options(); track option.value) {{\r\n" +
             $"        <mat-radio-button [value]=\"option.value\">{{{{option.displayName}}}}</mat-radio-button>\r\n" +
             $"    }}\r\n" +
             $"</mat-radio-group>\r\n" +
             $"}}\r\n");
 
-    private static IHtmlContent RenderMultiCheckboxField(this IHtmlHelper html, MultiCheckboxFormControl field) =>
-        html.Raw(
+    private static IHtmlContent RenderMultiCheckboxField(this IHtmlHelper htmlHelper, MultiCheckboxFormControl field) =>
+        htmlHelper.Raw(
             $"@if (!isHidden('{field.PropertyName}', {field.Visible.ToString().ToLower()})) {{\r\n" +
             $"<div class=\"{field.StackedClasses()}\">\r\n" +
-            $"    @for (let option of {field.PropertyName}Options(); track option.value) {{\r\n" +
+            $"    @for (option of {field.PropertyName}Options(); track option.value) {{\r\n" +
             $"        <mat-checkbox [value]=\"option.value\">{{{{option.displayName}}}}</mat-checkbox>\r\n" +
             $"    }}\r\n" +
             $"</div>\r\n" +
             $"}}\r\n");
 
-    private static IHtmlContent RenderDatepickerField(this IHtmlHelper html, DatepickerFormControl field)
+    private static IHtmlContent RenderDatepickerField(this IHtmlHelper htmlHelper, DatepickerFormControl field)
     {
         var pickerElementId = $"picker_{field.PropertyName}";
         var appearanceAttribute = field.Appearance.HasValue
@@ -171,7 +171,7 @@ public static class AngularSignalsFormViewHtmlHelperExtensions
         var placeholderAttribute = field.Placeholder.Value.HasContent()
             ? $" [placeholder]=\"'{field.Placeholder.Value}'\""
             : "";
-        return html.Raw(
+        return htmlHelper.Raw(
             $"@if (!isHidden('{field.PropertyName}', {field.Visible.ToString().ToLower()})) {{\r\n" +
             $"<mat-form-field class=\"{field.StackedClasses()}\"{appearanceAttribute}>\r\n" +
             $"    <mat-label>{field.Label.Value}</mat-label>\r\n" +
@@ -182,7 +182,7 @@ public static class AngularSignalsFormViewHtmlHelperExtensions
             $"}}\r\n");
     }
 
-    private static IHtmlContent RenderDateTimePickerField(this IHtmlHelper html, DateTimePickerFormControl field)
+    private static IHtmlContent RenderDateTimePickerField(this IHtmlHelper htmlHelper, DateTimePickerFormControl field)
     {
         var pickerElementId = $"picker_{field.PropertyName}";
         var appearanceAttribute = field.Appearance.HasValue
@@ -191,7 +191,7 @@ public static class AngularSignalsFormViewHtmlHelperExtensions
         var placeholderAttribute = field.Placeholder.Value.HasContent()
             ? $" [placeholder]=\"'{field.Placeholder.Value}'\""
             : "";
-        return html.Raw(
+        return htmlHelper.Raw(
             $"@if (!isHidden('{field.PropertyName}', {field.Visible.ToString().ToLower()})) {{\r\n" +
             $"<mat-form-field class=\"{field.StackedClasses()}\"{appearanceAttribute}>\r\n" +
             $"    <mat-label>{field.Label.Value}</mat-label>\r\n" +
@@ -202,30 +202,30 @@ public static class AngularSignalsFormViewHtmlHelperExtensions
             $"}}\r\n");
     }
 
-    private static IHtmlContent RenderRichTextField(this IHtmlHelper html, RichTextInputFormControl field)
+    private static IHtmlContent RenderRichTextField(this IHtmlHelper htmlHelper, RichTextInputFormControl field)
     {
         var editorTagName = $"entry-{field.Editor.ToString().ToLower()}";
-        return html.Raw(
+        return htmlHelper.Raw(
             $"@if (!isHidden('{field.PropertyName}', {field.Visible.ToString().ToLower()})) {{\r\n" +
             $"<{editorTagName} formControlName=\"{field.PropertyName}\" class=\"{field.StackedClasses()}\"></{editorTagName}>\r\n" +
             $"}}\r\n");
     }
 
-    private static IHtmlContent RenderGenericField(this IHtmlHelper html, FormControl field) =>
-        html.Raw(
+    private static IHtmlContent RenderGenericField(this IHtmlHelper htmlHelper, FormControl field) =>
+        htmlHelper.Raw(
             $"@if (!isHidden('{field.PropertyName}', {field.Visible.ToString().ToLower()})) {{\r\n" +
             $"<input formControlName=\"{field.PropertyName}\" class=\"{field.StackedClasses()}\">\r\n" +
             $"}}\r\n");
 
-    private static IHtmlContent RenderArrayField(this IHtmlHelper html, ArrayFormControl arrayControl)
+    private static IHtmlContent RenderArrayField(this IHtmlHelper htmlHelper, ArrayFormControl arrayControl)
     {
         var group = (FormControlGroup)arrayControl.FormControlGroup;
         var propertyName = arrayControl.PropertyName;
-        var innerControls = html.RenderFormControls(group.FormControls).ToString();
-        return html.Raw(
+        var innerControls = htmlHelper.RenderFormControls(group.FormControls).ToString();
+        return htmlHelper.Raw(
             $"@if (!isHidden('{propertyName}', {arrayControl.Visible.ToString().ToLower()})) {{\r\n" +
             $"<ng-container formArrayName=\"{propertyName}\">\r\n" +
-            $"    @for (let control of form.get('{propertyName}')?.controls ?? []; track $index) {{\r\n" +
+            $"    @for (control of form.get('{propertyName}')?.controls ?? []; track $index) {{\r\n" +
             $"        <ng-container [formGroupName]=\"$index\">\r\n" +
             innerControls +
             $"        </ng-container>\r\n" +
